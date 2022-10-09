6a6f7289447af45419b99af75f84e5430bfec34f
glitch <saniltison@gmail.com>
2022-10-09 17:12:10 -0400

    implemented argparse in code
    - remove the input functionality in the code and change code input format to CLI input with args
    
    -tested with SAT success
    
    EOC

--- src/project_folder.py
+++ src/project_folder.py
@@ -1,9 +1,16 @@
 #!/bin/python3
-
+import argparse
 from lib.resources import python_programming_evn
 
 
+parse = argparse.ArgumentParser(description='Create a new project folder')
 
+# first positional argument to accept the project name
+parse.add_argument('project_name', metavar='POS_ARG-1',nargs=1, type=str, help='name of the project folder')
+
+# take-in 1st positional argument and pass it into the function
+args_pyList = parse.parse_args()
+proj_name = args_pyList.project_name[0]
 
 
 
@@ -11,11 +18,11 @@ from lib.resources import python_programming_evn
 
 
 # ******** GET INPUT FROM USER USING THE CLI
-msg = f'Enter The Name Of The Project Folder'
-print(msg)
-proj_name = input('=>')
+# msg = f'Enter The Name Of The Project Folder'
+# print(msg)
+# proj_name = input('=>')
 
-print()
+# print()
 
 # msg = f'Enter The Location to Save the Project Folder - [Default = ~/home/$USER/Documents/]'
 # print(msg)
